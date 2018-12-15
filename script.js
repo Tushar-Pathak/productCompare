51c27d0a558b385a190abaaefbca9783abfca501
tusharpathak <tkp.tushar@gmail.com>
2018-12-15 16:37:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems, {});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".materialboxed");
+  var instances = M.Materialbox.init(elems, {});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".tooltipped");
+  var instances = M.Tooltip.init(elems, {});
+});
