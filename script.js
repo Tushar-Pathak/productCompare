3d2740297ec3fd2e4fc63d376db0216a4cf0eb81
tusharpathak <tkp.tushar@gmail.com>
2018-12-15 16:46:43 +0530

    little modifications

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll("select");
-  var instances = M.FormSelect.init(elems, {});
-});
+(function() {
+  document.addEventListener("DOMContentLoaded", function() {
+    const elems = document.querySelectorAll("select");
+    const instances = M.FormSelect.init(elems, {});
+  });
 
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".materialboxed");
-  var instances = M.Materialbox.init(elems, {});
-});
+  document.addEventListener("DOMContentLoaded", function() {
+    const elems = document.querySelectorAll(".materialboxed");
+    const instances = M.Materialbox.init(elems, {});
+  });
 
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".tooltipped");
-  var instances = M.Tooltip.init(elems, {});
-});
+  document.addEventListener("DOMContentLoaded", function() {
+    const elems = document.querySelectorAll(".tooltipped");
+    const instances = M.Tooltip.init(elems, {});
+  });
+})();
